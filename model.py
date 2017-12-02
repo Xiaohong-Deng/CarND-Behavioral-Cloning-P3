039f4f3320512a465ecfc6994b1c3b77ef31662f
Xiaohong-Deng <worldsky86rough@gmail.com>
2017-12-02 10:27:26 +0800

    use left and right images

--- model.py
+++ model.py
@@ -11,14 +11,30 @@ with open('./driving_log.csv') as csvfile:
 
 images = []
 measurements = []
+correction = 0.2
+
 for line in lines:
-  source_path = line[0]
-  filename = source_path.split('\\')[-1]
-  current_path = './IMG/' + filename
-  image = cv2.imread(current_path)
-  images.append(image)
+  center_path = line[0]
+  left_path = line[1]
+  right_path = line[2]
+
+  center_filename = center_path.split('\\')[-1]
+  left_filename = left_path.split('\\')[-1]
+  right_filename = right_path('\\')[-1]
+
+  current_center_path = './IMG/' + center_filename
+  current_left_path = './IMG/' + left_filename
+  current_right_path = './IMG' + right_filename
+
+  images.append(cv2.imread(current_center_path))
   measurement = float(line[3])
   measurements.append(measurement)
+
+  image.append(cv2.imread(current_left_path))
+  measurements.append(measurement + correction)
+
+  image.append(cv2.imread(current_right_path))
+  measurements.append(measurement - correction)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
@@ -32,6 +48,6 @@ model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True,
-          nb_epoch=7)
+          nb_epoch=1)
 
 model.save('model.h5')
